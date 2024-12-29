7d702ad6e8228bdf3288f034f3e9f389b6451774
KaitaTsuchiya <yukaigangan@gmail.com>
2024-12-29 11:23:59 +0900

    feat: メンション追加

--- src/slackNotifier.ts
+++ src/slackNotifier.ts
@@ -2,19 +2,37 @@ import axios from "axios";
 import dotenv from "dotenv";
 import { ProcessedProperty } from "./modules/processProperties";
 
+const watchPrefectures = ["千葉"]; //tdfk_name
+
+const hasPrefectureInProcessedProperty = (
+  processedProperties: ProcessedProperty[]
+): boolean =>
+  processedProperties.some((processedProperty) =>
+    processedProperty.prefectures.some((prefecture) =>
+      watchPrefectures.includes(prefecture.name)
+    )
+  );
+
 dotenv.config();
 
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
-export async function notifySlack(
-  properties: ProcessedProperty[]
-): Promise<void> {
+const postSlackCannel = async (message: string) => {
   if (!SLACK_WEBHOOK_URL) {
     console.error("Slack Webhook URLが設定されていません。");
     return;
   }
 
+  await axios.post(SLACK_WEBHOOK_URL, {
+    text: message,
+  });
+};
+
+export async function notifySlack(
+  properties: ProcessedProperty[]
+): Promise<void> {
   if (properties.length === 0) {
+    postSlackCannel("通知する物件情報がありません🥲");
     console.log("通知する物件情報がありません。");
     return;
   }
@@ -30,9 +48,12 @@ export async function notifySlack(
     .join("\n\n");
 
   try {
-    await axios.post(SLACK_WEBHOOK_URL, {
-      text: `🏠 *新しい物件情報* 🏠\n\n${message}`,
-    });
+    await postSlackCannel(
+      `${
+        hasPrefectureInProcessedProperty(properties) ? "<!channel> " : ""
+      }🏠 *新しい物件情報* 🏠\n\n${message}`
+    );
+
     console.log("Slack通知を送信しました。");
   } catch (error) {
     console.error("Slack通知の送信に失敗しました:", error);
