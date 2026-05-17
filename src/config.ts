79cf12a10eaf52a4aab4c236ccf702ae13a637b3
KaitaTsuchiya <yukaigangan@gmail.com>
2026-05-17 14:35:24 +0900

    feat: 対象の都道府県を変更

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,8 @@
-export const WATCH_PREFECTURES: string[] = ["東京", "千葉", "北海道", "愛知"];
+export const WATCH_PREFECTURES: string[] = [
+  "東京",
+  "千葉",
+  "北海道",
+  "大阪",
+  "兵庫",
+];
 export const MIN_FLOOR = 3;
